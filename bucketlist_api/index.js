819c87571fcf312d1731ade730907bdaa02460eb
thegirlfromipanema <mayanovarini@gmail.com>
2017-11-28 02:34:26 -0800

    add post router

--- bucketlist_api/index.js
+++ bucketlist_api/index.js
@@ -1,15 +1,19 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
-    
+    port = process.env.PORT || 3030,
+    bodyParser = require('body-parser');
+
 var bucketlistRoutes = require('./routes/bucketlist');
-    
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function(req, res){
     res.send("Hello from the root route!");
 });
 
 app.use('/api/bucketlist', bucketlistRoutes);
-    
+
 app.listen(port, function(){
     console.log("App is running on port " + process.env.PORT);
 })
